723826679d93963690786fa5768ff01362b9a23b
JamesRobert20 <jmtendamema@gmail.com>
2024-03-05 22:15:15 -0700

    moved public to root
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,5 +1,19 @@
+import styles from './page.module.css'
+import '@/'
+
+const activities = [
+    {
+        id: 1,
+        name: 'Banff Skiing',
+        rating: 4.5,
+        picture: '~/assets/banff.jpg'
+    }
+]
+
 export default function Activities() {
     return (
-        <div>pages</div>
+        <div className={styles.container}>
+
+        </div>
     )
 }
