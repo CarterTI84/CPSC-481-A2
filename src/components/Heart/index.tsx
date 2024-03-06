692d34d8b70c4d818d443634fb7116e23e279116
JamesRobert20 <jmtendamema@gmail.com>
2024-03-05 23:44:32 -0700

    Completed activities page
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

---
+++ src/components/Heart/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Favorite } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import { useState } from 'react';
+
+export default function index() {
+    const [clicked, setClicked] = useState(false);
+    const toggleState = () => {
+        setClicked(clicked => !clicked)
+    }
+
+    return (
+        <IconButton onClick={toggleState}>
+            <Favorite htmlColor={clicked ? 'pink' : 'grey'} />
+        </IconButton>
+    )
+}
