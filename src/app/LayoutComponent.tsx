1e373dcb1f50fc93ba704de48ee58b9f80e8e6f1
JamesRobert20 <jmtendamema@gmail.com>
2024-04-08 00:41:52 -0600

    Saving activities complete
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- src/app/LayoutComponent.tsx
+++ src/app/LayoutComponent.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ActivitiesContextProvider } from "@/contexts/activitiesContext"
 import { LoadingContextProvider } from "@/contexts/loadingContext"
 import { ScheduleContextProvider } from "@/contexts/scheduleContext"
 import { motion, AnimatePresence } from "framer-motion"
@@ -10,9 +11,11 @@ export default function LayoutComponent({ children }: PropsWithChildren) {
         <AnimatePresence>
             <motion.div className="beatifulScroll" style={{ height: "calc(100vh - 80px)" }}>
                 <LoadingContextProvider>
-                    <ScheduleContextProvider>
-                        {children}
-                    </ScheduleContextProvider>
+                    <ActivitiesContextProvider>
+                        <ScheduleContextProvider>
+                            {children}
+                        </ScheduleContextProvider>
+                    </ActivitiesContextProvider>
                 </LoadingContextProvider>
             </motion.div>
         </AnimatePresence>
